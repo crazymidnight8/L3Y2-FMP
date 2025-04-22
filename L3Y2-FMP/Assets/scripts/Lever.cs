e69b5b32f785d9bf84ae9c42ee61569aa07931bc
crazymidnight5 <167751030+crazymidnight5@users.noreply.github.com>
2025-04-22 12:05:01 +0100

    water cave, grove, ui
    finished water cave area,
    started work on grove,
    added ui sliders for if levers are active

--- L3Y2-FMP/Assets/scripts/Lever.cs
+++ L3Y2-FMP/Assets/scripts/Lever.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Lever : MonoBehaviour
 {
 
     public bool isOn;
+    public float LeverValue;
+    public float LeverMax;
 
     public float moveSpeed;
     public Transform bridge;
     public Transform pointA;
     public Transform pointB;
 
+    public Slider IsOff;
+
     // Start is called before the first frame update
     void Start()
     {
         isOn = false;
+        LeverValue = 0f;
+        LeverMax = 0f;
     }
 
     // Update is called once per frame
@@ -26,10 +34,14 @@ public class Lever : MonoBehaviour
         if (isOn)
         {
             bridge.position = Vector3.Lerp(bridge.position, pointB.position, moveSpeed * Time.deltaTime);
+            LeverValue = 1f;
+            IsOff.value = LeverValue;
         }
         else
         {
             bridge.position = Vector3.Lerp(bridge.position, pointA.position, moveSpeed * Time.deltaTime);
+            LeverValue = 0f;
+            IsOff.value = LeverValue;
         }
     }
 }
